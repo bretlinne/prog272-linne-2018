9327ce997cd0344285ea0a9aa729b84a30a5f9b0
Your Name <you@example.com>
2018-06-14 15:23:03 -0700

    latest progress

--- final-project-address-master/src/Address.test.js
+++ final-project-address-master/src/Address.test.js
@@ -2,9 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import dataManager from '../assets/FakeDataManager'
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Address from './components/Address'
+import addresses from './address-list'
+
+let  wrapper = null;
+
+configure({ adapter: new Adapter() })
+
+beforeEach(() => {
+    wrapper = shallow(<Address
+        dataManager={dataManager}
+        addressList={addresses} />);
+});
+
+const addressProp = wrapper => wrapper
+    .find('WithStyles(AddressShow)')
+    .prop('address');
+
+it('renders and displays the default first name', () => {
+    expect(addressProp(wrapper).firstName).toEqual('unknown');
+});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
+
+
